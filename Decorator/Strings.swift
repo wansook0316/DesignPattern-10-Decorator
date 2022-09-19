9976163be4bf6107b2b54f88cd3591f309d912e0
wansook0316 <wansook0316@gmail.com>
2022-09-19 10:08:04 +0900

    Create: Strings

---
+++ Decorator/Strings.swift
@@ -0,0 +1,47 @@
+//
+//  Strings.swift
+//  Decorator
+//
+//  Created by Choiwansik on 2022/09/19.
+//
+
+import Foundation
+
+internal struct Strings {
+    internal var lineCount: Int {
+        self.strings.count
+    }
+
+    internal var maxLength: Int {
+        self.strings
+            .map { $0.count }
+            .max() ?? .zero
+    }
+
+    internal func add(item: String) {
+        self.strings.append(item)
+    }
+
+    private var strings = [String]()
+
+}
+
+extension Strings: Item {
+
+    internal func length(of index: Int) -> Int {
+        guard index < self.lineCount else {
+            return
+        }
+
+        return self.strings[index].count
+    }
+
+    internal func string(of index: Int) -> String {
+        guard index < self.lineCount else {
+            return
+        }
+
+        return self.strings[index]
+    }
+
+}
